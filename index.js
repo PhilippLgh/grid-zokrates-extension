96bc864cb0eee5fff39124e175c98ba1098292a9
philipplgh <philipplgh@gmail.com>
2019-05-24 15:47:23 +0200

    init

---
+++ index.js
@@ -0,0 +1,8 @@
+const platform = process.platform === 'win32' ? 'windows' : process.platform
+
+module.exports = {
+  name: 'zokrates',
+  displayName: 'ZoKrates',
+  type: 'tools',
+  repository: 'https://github.com/Zokrates/ZoKrates'
+}
